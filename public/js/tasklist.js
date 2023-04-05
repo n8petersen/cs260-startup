ce5f178dcc303fccdab4d5a6428cf29ff7c4d85b
npetersen <npetersen@goldpointsystems.com>
2023-04-05 12:59:04 -0600

    Restyled some buttons

--- public/js/tasklist.js
+++ public/js/tasklist.js
@@ -12,7 +12,7 @@ class Task {
                 <input type="checkbox" id="${ this.id }" class="form-check-input" onclick="updateTask('${ this.id }')">
                 <label for="${ this.id }" class="task-description">${ this.text }</label>
                 <label for="${ this.id }" class="task-date">${ this.prettyDate() }</label>
-                <button class="material-icon trash-can"onclick="deleteTask('${ this.id }')">delete</button>
+                <button class="material-icon trash-can float-end" onclick="deleteTask('${ this.id }')">delete</button>
             </li>
             `
         }
@@ -23,7 +23,7 @@ class Task {
                 <input type="checkbox" id="${ this.id }" class="form-check-input" checked onclick="updateTask('${ this.id }')">
                 <label for="${ this.id }" class="task-description checked">${ this.text }</label>
                 <label for="${ this.id }" class="task-date">${ this.prettyDate() }</label>
-                <button class="material-icon trash-can" onclick="deleteTask('${ this.id }')">delete</button>
+                <button class="material-icon trash-can float-end" onclick="deleteTask('${ this.id }')">delete</button>
             </li>
             `
         }
